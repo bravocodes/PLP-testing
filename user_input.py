780cbb2fe50f6a5d8febf4e2de0e08ee5046cdc1
bravocodes <137946218+bravocodes@users.noreply.github.com>
2024-02-29 20:36:17 +0300

    First python assignment

---
+++ user_input.py
@@ -0,0 +1,5 @@
+#create a program that takes user name age & loc
+name=input("Enter you name:")
+age=input("Enter your age:")
+location=input("Enter your location:")
+print("Hello",name, " ,you are ",age,"years old and live in ",location)
