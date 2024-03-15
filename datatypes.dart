240e964feb94806c7134325daf952ce1b26ff9d8
bravocodes <137946218+bravocodes@users.noreply.github.com>
2024-03-15 11:14:33 +0300

    Add files via upload

---
+++ datatypes.dart
@@ -0,0 +1,11 @@
+int main() {
+var a=63; //is an integer data type
+var b=44.56; //is a double data type
+var c="Keith"; //is a string data type
+var d=["Audi","Mercedes","BMW","Toyota"];//is a list data type
+print("$a is a integer dara type");
+print("$b is a double data type");
+print("$c is a string data type");
+print("$d is a list data type");
+return 0;
+}
