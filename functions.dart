e15acf3d31ed05c219700a50b75c7e439fb897d4
bravocodes <137946218+bravocodes@users.noreply.github.com>
2024-03-15 11:57:05 +0300

    Add files via upload

---
+++ functions.dart
@@ -0,0 +1,29 @@
+        int addTwo(int a , int b, int sum){
+        sum=a + b ;
+        return sum;
+    }
+    int subtractTwo( int a, int b, int difference){
+        difference= a -  b;
+        return difference;
+    }
+    int multiplyTwo(int a, int b,int product){
+        product= a* b;
+        return product;
+    }
+    int divideTwo( int a, int b,int division){
+        division= (a / b) as int;
+        return division;
+    }
+    int stringLength(String string1,int length){
+    String string1="Lydiah";
+    return string1.length;
+    }
+    int getFirstElement(list1){
+        if (list1.isEmpty){
+            return 0;
+        }
+        return list1[0];
+    }
+    void main(){
+      
+    }
